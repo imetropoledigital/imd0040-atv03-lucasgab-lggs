8c37d4329fe1b430bd2b92d42bfb5daa8a6e839d
Lucas Gabriel <lucasgab.lggs@gmail.com>
2024-04-04 14:48:26 -0300

    Realização da atividade 3.

---
+++ src/main/java/br/ufrn/imd/Musician.java
@@ -0,0 +1,15 @@
+package br.ufrn.imd;
+
+public class Musician extends Person {
+    public boolean playClassicalMusic;
+    public boolean canImprovise;
+    public boolean canCompose;
+
+    Musician() {
+
+    }
+
+    public void play() {
+
+    }
+}
